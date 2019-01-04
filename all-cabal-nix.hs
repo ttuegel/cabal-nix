2a717cfd5d481993275cf3d022e6ad6dd7052b2d
Thomas Tuegel <ttuegel@mailbox.org>
2019-01-04 11:14:51 -0600

    Write each package version to a separate file

--- all-cabal-nix.hs
+++ all-cabal-nix.hs
@@ -2,6 +2,7 @@ module Main (main) where
 
 import Control.Concurrent (QSem)
 import Control.Concurrent.Async (Concurrently)
+import Data.Foldable
 import Data.Map.Strict (Map)
 import Data.Set (Set)
 import Distribution.Compiler (CompilerInfo)
@@ -18,6 +19,8 @@ import qualified Control.Concurrent as Concurrent
 import qualified Control.Concurrent.Async as Async
 import qualified Control.Exception as Exception
 import qualified Control.Monad as Monad
+import qualified Control.Monad.State.Strict as State
+import qualified Data.Aeson as Aeson
 import qualified Data.Map.Strict as Map
 import qualified Data.Set as Set
 import qualified Distribution.Pretty as Pretty
@@ -27,6 +30,7 @@ import qualified Pipes as Pipes
 import qualified Pipes.Aeson.Unchecked as Pipes.Aeson
 import qualified Pipes.ByteString as Pipes.ByteString
 import qualified System.Directory as Directory
+import qualified System.IO as IO
 
 import Orphans ()
 import Package (Package)
@@ -71,13 +75,12 @@ withQSem qsem =
         (Concurrent.signalQSem qsem)
 
 forPlatforms
-    :: QSem
-    -> FilePath -- ^ Cabal hashes
+    :: FilePath -- ^ Cabal hashes
     -> CompilerInfo  -- ^ Selected compiler
     -> PackageId
-    -> Concurrently (Map Platform Package)
-forPlatforms qsem allCabalHashes compilerInfo packageId =
-    concurrently qsem (Package.forPlatforms compilerInfo platforms cabalFile)
+    -> IO (Map Platform Package)
+forPlatforms allCabalHashes compilerInfo packageId =
+     (Package.forPlatforms compilerInfo platforms cabalFile)
   where
     cabalFile =
         allCabalHashes </> package </> version </> package <.> "cabal"
@@ -85,6 +88,51 @@ forPlatforms qsem allCabalHashes compilerInfo packageId =
         PackageIdentifier { pkgName, pkgVersion } = packageId
         package = Pretty.prettyShow pkgName
         version = Pretty.prettyShow pkgVersion
+
+writePackageJSON
+    :: QSem
+    -> FilePath
+    -> CompilerInfo
+    -> PackageId
+    -> Concurrently ()
+writePackageJSON qsem allCabalHashes compilerInfo packageId =
+    concurrently qsem
+        (IO.withFile (packageJSON allCabalHashes packageId) IO.WriteMode
+              (\hndl ->
+                do
+                  package <- forPlatforms allCabalHashes compilerInfo packageId
+                  let outp = Pipes.ByteString.toHandle hndl
+                  Pipes.runEffect (Pipes.Aeson.encode package >-> outp)
+              )
+        )
+
+packageJSON :: FilePath -> PackageId -> FilePath
+packageJSON allCabalHashes packageId@PackageIdentifier { pkgName } =
+    allCabalHashes
+        </> Pretty.prettyShow pkgName
+        </> Pretty.prettyShow packageId <.> "json"
+
+readPackageJSON
+    :: QSem
+    -> FilePath
+    -> PackageId
+    -> Concurrently Aeson.Object
+readPackageJSON qsem allCabalHashes packageId =
+    concurrently qsem
+        (IO.withFile (packageJSON allCabalHashes packageId) IO.ReadMode
+            (\hndl ->
+              do
+                let inp = Pipes.ByteString.fromHandle hndl
+                r <- State.evalStateT Pipes.Aeson.decode inp
+                case r of
+                  Nothing ->
+                      error "Not enough input"
+                  Just (Left decodeError) ->
+                      error (Exception.displayException decodeError)
+                  Just (Right object) ->
+                      return object
+            )
+        )
 
 getPackageIds
     :: FilePath  -- ^ Cabal hashes
@@ -123,14 +171,19 @@ main =
   do
     Options { allCabalHashes, compilerInfo } <- Options.execParser parserInfo
     packages <- getPackages allCabalHashes
-    allPackageIds <-
-        Set.unions <$> traverse (getPackageIds allCabalHashes) packages
     njobs <- Concurrent.getNumCapabilities
     qsem <- Concurrent.newQSem njobs
+    allPackageIds <-
+        mconcat <$> traverse (getPackageIds allCabalHashes) packages
+    Async.runConcurrently
+        (traverse_
+            (writePackageJSON qsem allCabalHashes compilerInfo)
+            allPackageIds
+        )
     allPackages <-
         Async.runConcurrently
             (traverse
-                (forPlatforms qsem allCabalHashes compilerInfo)
+                (readPackageJSON qsem allCabalHashes)
                 (Map.fromSet id allPackageIds)
             )
     Pipes.runEffect
